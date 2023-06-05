a9ff85efad10bd842539f9c913309ec24759d07e
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-06-05 17:24:08 -0600

    Update README and schema based on prev PR

--- schema.sql
+++ schema.sql
@@ -1,10 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
+DROP TABLE IF EXISTS animals;
+
 CREATE TABLE animals(
 	id INT  GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(40) NOT NULL,
 	date_of_birth DATE NOT NULL,
 	escape_attempts INT NOT NULL,
 	neutered BOOLEAN NOT NULL,
-	weight_kg DECIMAL(5,2) NOT NULL
+	weight_kg DECIMAL(5,2) NOT NULL,
+	PRIMARY KEY(id)
 );
