a3dc9367b2dc892b5c24021c4fe41a281fef4ed1
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-06-05 19:03:47 -0600

    Fix typo

--- queries.sql
+++ queries.sql
@@ -20,7 +20,7 @@ SELECT * FROM animals;
 
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-UPDATE animals SET species = 'pokimon' WHERE species IS NULL;
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 SELECT * FROM animals;
 COMMIT;
 SELECT * FROM animals;
