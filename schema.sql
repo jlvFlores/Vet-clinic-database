b388c7e6901a769a152c873faf4fd01a31247e10
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-06-06 11:25:15 -0600

    Add owners and species tables and Add FKs to animals

--- schema.sql
+++ schema.sql
@@ -15,3 +15,27 @@ CREATE TABLE animals(
 
 -- DAY 2
 ALTER TABLE animals ADD species VARCHAR(20);
+
+--DAY 3
+DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS species;
+
+CREATE TABLE owners(
+	id INT  GENERATED ALWAYS AS IDENTITY,
+	full_name VARCHAR(100) NOT NULL,
+	age INT NOT NULL,
+	PRIMARY KEY(id)
+);
+CREATE TABLE species(
+	id INT  GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(20) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD CONSTRAINT FK_AnimalSpecies FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT FK_AnimalOwner FOREIGN KEY (owner_id) REFERENCES owners(id);
